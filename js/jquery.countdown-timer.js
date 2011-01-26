2e9e4d0138013cfc7959065aaeaab19a153888f9
Duncan McMillan <duncan@artandsoul.co.uk>
2011-01-26 14:39:29 +0000

    Added configuration options for number of significant days and custom separator

--- js/jquery.countdown-timer.js
+++ js/jquery.countdown-timer.js
@@ -9,22 +9,24 @@
 (function($){
 	
 	var options = {
-		
+		display_as_text		: false,
+		to					: '2025:06:14:00:01',
+		separator			: ':',
+		significant_days	: 3
 	};
 	
 	var tick_interval;
 	var element;
 	var seconds_remaining;
-	var separator = ':';
 	
-	$.fn.counter = function (config)
+	$.fn.countdown = function (config_options)
 	{
 		
-		var initialise = function (end, container)
+		var initialise = function (container)
 		{
 			element = container;
 			
-			var date_elements = end.split(':');
+			var date_elements = options.to.split(':');
 		
 			var end_date = new Date (date_elements[0], date_elements[1] - 1, date_elements[2], date_elements[3], date_elements[4]);
 			var start_date = new Date();
@@ -39,25 +41,63 @@
 			seconds_remaining --;
 			
 			if (seconds_remaining >= 0) {
-				update();
+				update(seconds_remaining);
 			} else {
 				clearInterval(tick_interval);
 			}		
 		};
 		
-		var update = function()
+		var update = function (seconds_remaining)
+		{
+			element.html(assemble_time_str(seconds_remaining));
+		}
+		
+		var assemble_time_str = function (seconds_remaining)
 		{
 			time_array = new Array();
 			
-			time_array.push(Math.floor(seconds_remaining / 86400));
-			time_array.push(Math.floor((seconds_remaining % 86400) / 3600));
-			time_array.push(Math.floor((seconds_remaining % 3600) / 60));
-			time_array.push(Math.floor(seconds_remaining % 60));
+			var day = String(Math.floor(seconds_remaining / 86400));
 			
-			 element.html(time_array.join(separator));
+			if (String(day).length > options.significant_days){
+				time_array.push(day.substr(day.length - options.significant_days));
+			} else {
+				time_array.push(str_pad(day, options.significant_days, "0"));
+			}
+			
+			var hrs = String(Math.floor((seconds_remaining % 86400) / 3600));
+			time_array.push(str_pad(hrs, 2, "0"));
+			
+			var min = String(Math.floor((seconds_remaining % 3600) / 60));
+			time_array.push(str_pad(min, 2, "0"));
+			
+			var sec = String(Math.floor(seconds_remaining % 60));
+			time_array.push(str_pad(sec, 2, "0"));
+			
+			return time_array.join(options.separator);
 		};
 		
-		initialise(config.end, this);
+		var str_pad = function (input, len, str)
+		{
+			var padding = '';
+			
+			if (str == null) {
+				str = "0";
+			}
+			
+			if (len == null) {
+				len = input.length + 1;
+			}
+			
+			for (i = input.length; i < len; i++)
+			{
+				padding += str;
+			}
+			
+			return padding + input;
+		}
+		
+		$.extend(options, config_options);
+		initialise(this);
 		
 	
 	};
